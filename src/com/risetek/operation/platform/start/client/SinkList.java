5e8ea6f5d43cac514c047f60fa8d762e922e3d69
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-23 16:09:37 +0000

    增加类个方法的注释

--- src/com/risetek/operation/platform/start/client/SinkList.java
+++ src/com/risetek/operation/platform/start/client/SinkList.java
@@ -2,9 +2,20 @@ package com.risetek.operation.platform.start.client;
 
 import com.risetek.operation.platform.base.client.BaseSink;
 
+/**
+ * @author Amber
+ * 功能：模块列表类
+ * 2010-8-23 下午11:59:28
+ */
 @SuppressWarnings("rawtypes")
 public class SinkList {
 	
+	/**
+	 * 功能：注册功能模块
+	 *
+	 * Class[]
+	 * @return 返回功能模块列表
+	 */
 	public static Class[] getSinkList(){
 		Class[] sinkList = new Class[]{
 			BaseSink.class	
--- src/com/risetek/operation/platform/start/client/StartUp.java
+++ src/com/risetek/operation/platform/start/client/StartUp.java
@@ -6,16 +6,37 @@ import com.google.gwt.user.client.ui.TreeItem;
 import com.risetek.operation.platform.base.client.BaseSink;
 import com.risetek.operation.platform.launch.client.OplatformLaunch;
 import com.risetek.operation.platform.launch.client.sink.SinkInfo;
+import com.risetek.operation.platform.process.client.ProcessSink;
 
+/**
+ * @author Amber
+ * 功能：整个项目的入口
+ * 2010-8-23 下午11:56:43
+ */
 public class StartUp extends OplatformLaunch {
 	
+	/** 
+	 * 功能： 菜单注册
+	 *(non-Javadoc)
+	 * @see com.risetek.operation.platform.launch.client.OplatformLaunch#registerTreeMenu(com.google.gwt.user.client.ui.Tree)
+	 */
 	@Override
 	public Tree registerTreeMenu(Tree userMenu) {
-		userMenu = addTreeItem(userMenu, BaseSink.init());
+		userMenu.addItem(addTreeItem(userMenu, BaseSink.init()));
+		userMenu.addItem(addTreeItem(userMenu, ProcessSink.init()));
 		return userMenu;
 	}
 	
-	private Tree addTreeItem(Tree tree, SinkInfo info){
+	/**
+	 * 功能：添加节点
+	 *
+	 * TreeItem
+	 * @param tree
+	 * @param info
+	 * @return
+	 */
+	private TreeItem addTreeItem(Tree tree, SinkInfo info){
+		TreeItem addItem = null;
 		String group = info.getGroup();
 		int count = tree.getItemCount();
 		boolean added = false;
@@ -30,16 +51,22 @@ public class StartUp extends OplatformLaunch {
 				}
 			}
 			if(!added){
-				TreeItem item = creatGrpupTreeItem(group, info);
-				tree.addItem(item);
+				addItem = creatGrpupTreeItem(group, info);
 			}
 		} else {
-			TreeItem item = creatGrpupTreeItem(group, info);
-			tree.addItem(item);
+			addItem = creatGrpupTreeItem(group, info);
 		}
-		return tree;
+		return addItem;
 	}
 	
+	/**
+	 * 功能：创建分组节点
+	 *
+	 * TreeItem
+	 * @param group
+	 * @param info
+	 * @return
+	 */
 	private TreeItem creatGrpupTreeItem(String group, SinkInfo info){
 		TreeItem item = new TreeItem(group);
 		TreeItem childItem = creatChildTreeItem(info);
@@ -47,11 +74,18 @@ public class StartUp extends OplatformLaunch {
 		return item;
 	}
 	
+	/**
+	 * 功能：创建功能节点
+	 *
+	 * TreeItem
+	 * @param info
+	 * @return
+	 */
 	private TreeItem creatChildTreeItem(SinkInfo info){
 		TreeItem childItem = new TreeItem();
 		childItem.setText(info.getName());
 		childItem.setTitle(info.getDescription());
-		childItem.setUserObject(info);
+		childItem.setUserObject(info.getInstance().getWidget());
 		return childItem;
 	}
 }
