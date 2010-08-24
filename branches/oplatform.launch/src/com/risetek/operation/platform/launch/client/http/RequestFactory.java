073206aec0aa57257e402af51cfa21b13693067f
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-24 10:57:38 +0000

    增加http请求工厂

---
+++ branches/oplatform.launch/src/com/risetek/operation/platform/launch/client/http/RequestFactory.java
@@ -0,0 +1,99 @@
+package com.risetek.operation.platform.launch.client.http;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONString;
+
+public class RequestFactory {
+
+	private final String baseUrl;
+	private Request request;
+	private String SIGNATURE = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+	
+	public RequestFactory(){
+		this.baseUrl = "http://192.168.6.9:8089/billServer/billCenter!request.do";
+	}
+	
+	private class hookRequestCallback implements RequestCallback {
+
+		private RequestCallback hooker;
+		
+		public hookRequestCallback(RequestCallback handler){
+			hooker = handler;
+		}
+		
+		@Override
+		public void onResponseReceived(Request request, Response response) {
+			hooker.onResponseReceived(request, response);
+		}
+
+		@Override
+		public void onError(Request request, Throwable exception) {
+			hooker.onError(request, exception);
+		}
+		
+	}
+	
+	public void get(String path, String query, RequestCallback handler){
+		if(request != null && request.isPending()){
+			request.cancel();
+		}
+		String url = baseUrl + path;
+		if(query != null){
+			url += "?BILLS_REQUEST=" + query;
+		}
+		JSONObject root = new JSONObject();
+		JSONString actionName = new JSONString("SELECT_CUSTOMER");
+		root.put("ACTION_NAME", actionName);
+		JSONObject info = new JSONObject();
+		JSONString value = new JSONString("200");
+		info.put("CUSTOMER_ID", value);
+		root.put("ACTION_INFO", info);
+		url += root.toString() + SIGNATURE;
+		System.out.println(url);
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, url);
+		builder.setTimeoutMillis(30000);
+		builder.setHeader("Content-Type", "text/plain; charset=UTF-8");
+		try{
+			request = builder.sendRequest(null, new hookRequestCallback(handler));
+		} catch (RequestException e){ 
+			GWT.log( "error", e); 
+		}
+	}
+	
+	public void get0(String path, String query, RequestCallback handler) {
+		if (request != null && request.isPending()) {
+			request.cancel();
+		}
+		RequestBuilder builder;
+
+		String url = baseUrl + path;
+		if(query != null){
+			url += "?BILLS_REQUEST=" + query;
+		}
+		JSONObject root = new JSONObject();
+		JSONString actionName = new JSONString("SELECT_CUSTOMER");
+		root.put("ACTION_NAME", actionName);
+		JSONObject info = new JSONObject();
+		JSONString value = new JSONString("200");
+		info.put("CUSTOMER_ID", value);
+		root.put("ACTION_INFO", info);
+		url += root.toString() + SIGNATURE;
+		System.out.println(url);
+		builder = new RequestBuilder(RequestBuilder.GET, url);
+
+		builder.setTimeoutMillis(3000);
+		builder.setHeader("Content-Type", "text/plain;");
+
+		try {
+			request = builder.sendRequest(null, handler);
+		} catch (RequestException e) {
+			GWT.log("error", e);
+		}
+	}
+}
