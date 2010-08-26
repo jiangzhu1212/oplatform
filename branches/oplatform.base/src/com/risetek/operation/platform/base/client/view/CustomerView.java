ccd2938c33216dd5a2e0e867208cf86fb910a473
onlyyouabcd <onlyyouabcd@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-26 09:49:01 +0000

    OOXX

--- branches/oplatform.base/src/com/risetek/operation/platform/base/client/view/CustomerView.java
+++ branches/oplatform.base/src/com/risetek/operation/platform/base/client/view/CustomerView.java
@@ -14,10 +14,10 @@ import com.risetek.operation.platform.base.client.CustomerSink;
 import com.risetek.operation.platform.base.client.control.CustomerController;
 import com.risetek.operation.platform.base.client.entry.CustomerConstanst;
 import com.risetek.operation.platform.base.client.model.CustomerData;
-import com.risetek.operation.platform.base.client.utils.YBUtil;
 import com.risetek.operation.platform.launch.client.config.UIConfig;
 import com.risetek.operation.platform.launch.client.view.IOPlatformView;
 import com.risetek.operation.platform.launch.client.view.OPlatformTableView;
+import com.risetek.operation.platform.launch.shared.FieldVerifier;
 
 public class CustomerView extends OPlatformTableView implements IOPlatformView {
 
@@ -82,7 +82,7 @@ public class CustomerView extends OPlatformTableView implements IOPlatformView {
 			@Override
 			public void onClick(ClickEvent event) {
 				// TODO Auto-generated method stub
-				int row = YBUtil.getCheckedRow(grid);
+				int row = FieldVerifier.getCheckedRow(grid);
 				if(row != 0){
 					Window.alert(""+row);
 				}
