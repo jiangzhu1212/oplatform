fe2b3e11bcfe6230187ac012392c2e832b7f7aa8
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-26 07:30:54 +0000

    新增对话框的基类 完善整个页面风格和布局

---
+++ src/com/risetek/operation/platform/launch/client/dialog/CustomDialog.java
@@ -0,0 +1,65 @@
+package com.risetek.operation.platform.launch.client.dialog;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CustomDialog extends DialogBox {
+
+	private static CustomDialogUiBinder uiBinder = GWT
+			.create(CustomDialogUiBinder.class);
+
+	interface CustomDialogUiBinder extends UiBinder<Widget, CustomDialog> {
+	}
+
+	@UiField protected Label label;
+
+	@UiField protected VerticalPanel mainPanel;
+	
+	@UiField protected Label message;
+	
+	@UiField public Button submit;
+	
+	@UiField Button cancel;
+	
+	public CustomDialog() {
+		setWidget(uiBinder.createAndBindUi(this));
+		setGlassEnabled(true);
+		submit.setStyleName("dialogButton");
+		cancel.setStyleName("dialogButton");
+		submit.setTabIndex(1001);
+		cancel.setTabIndex(1002);
+		setStyleName("dialog");
+		label.setStyleName("description");
+		mainPanel.setStyleName("main");
+	}
+	
+	protected void setDescript(String desc) {
+		label.setText(desc);
+	}
+	
+	protected void setMessage(String msg) {
+		message.setText(msg);
+	}
+
+	public void show(){
+		super.show();
+		center();
+	}
+	
+	@UiHandler("cancel")
+	void onClickCancel(ClickEvent e){
+		hide();
+	}
+	
+	public boolean onKeyDownPreview(char key, int modifiers){
+		return true;
+	}
+}
---
+++ src/com/risetek/operation/platform/launch/client/dialog/NoticeDialog.java
@@ -0,0 +1,21 @@
+package com.risetek.operation.platform.launch.client.dialog;
+
+import com.google.gwt.user.client.ui.Grid;
+
+public class NoticeDialog extends CustomDialog {
+
+	public NoticeDialog(String notice){
+		Grid grid = new Grid(1, 1);
+		grid.setText(0, 0, notice);
+		grid.setWidth("300px");
+		mainPanel.add(grid);
+		setText("通知公告");
+		setDescript("查看通知公告详细内容");
+	}
+	
+	public void show(){
+		cancel.setText("关闭");
+		submit.setVisible(false);
+		super.show();
+	}
+}
