011330d790da6703f6b242e43c9173d8f15c5c51
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-27 05:17:51 +0000

    增加子表

--- src/com/risetek/operation/platform/base/client/view/RoleConfigView.java
+++ src/com/risetek/operation/platform/base/client/view/RoleConfigView.java
@@ -1,17 +1,21 @@
 package com.risetek.operation.platform.base.client.view;
 
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.xml.client.Node;
-import com.risetek.operation.platform.launch.client.config.UIConfig;
+import com.risetek.operation.platform.base.client.RoleConfigSink;
 import com.risetek.operation.platform.launch.client.view.IOPlatformView;
 import com.risetek.operation.platform.launch.client.view.OPlatformTableView;
 
 public class RoleConfigView extends OPlatformTableView implements IOPlatformView {
 
-	public final static String[] columns = {"ID", "角色名称", "可操作内容"};
+	public final static String[] columns = {"ID", "角色名称", ""};
 	public final static int[] columnsWidth = {10, 30, 60};
-	public final static int rowCount = UIConfig.TABLE_ROW_NORMAL;
+	public final static int rowCount = 2;//UIConfig.TABLE_ROW_NORMAL;
 	public static String descript = "";
+	public Grid grid1 = new Grid();
 	
 	private final static String[] banner_text = {
 		"点击修改列1.",
@@ -22,7 +26,22 @@ public class RoleConfigView extends OPlatformTableView implements IOPlatformView
 	};
 	
 	public RoleConfigView(){
-		
+		grid.setHeight("40%");
+		addActionPanel(initPromptGrid(), RoleConfigSink.Desc);
+		setLocation(RoleConfigSink.Group + "->" + RoleConfigSink.Name);
+		grid1.setWidth("100%");
+		grid1.resize(4, 5);
+		grid1.setText(0, 0, "内容");
+		grid1.setStyleName("optable");
+		HTML title = new HTML("这是上面一张表的扩展内容");
+		title.setStyleName("tableordertitle");
+		outer.add(title);
+		outer.add(grid1);
+	}
+	
+	private Widget initPromptGrid(){
+		HorizontalPanel actionPanel = new HorizontalPanel();
+		return actionPanel;
 	}
 	
 	@Override
