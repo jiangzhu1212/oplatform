148ab47976e3296ebc79915b4e3ec44b68380a28
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-27 17:50:23 +0000

    扩充方法

--- branches/oplatform.launch/src/com/risetek/operation/platform/launch/client/util/Util.java
+++ branches/oplatform.launch/src/com/risetek/operation/platform/launch/client/util/Util.java
@@ -81,4 +81,50 @@ public class Util {
 		return 0;
 	}
 	
+	/**
+	 * @Description: 公共验证不能为空的字段
+	 * @param text
+	 * @return  参数 
+	 * @return String 返回类型
+	 */
+	public static String commValidity(String text, String title) {
+		if (null == text || "".equals(text)) {
+			return title + "不能为空！";
+		}
+		return null;
+	}
+	
+	/**
+	 * 日期比较函数 little小于或等于big日期返回true
+	 * @param little 小日期
+	 * @param big 大日期
+	 * @return 小日期小于大日期返回true，否则返回false
+	 */
+	public static boolean lessOrEqualThanDate(String little, String big) {
+		String[] littleArray = little.split("-");
+		String[] bigArray = big.split("-");
+		int yearLittle = Integer.parseInt(littleArray[0]);
+		int monthLittle = Integer.parseInt(littleArray[1]);
+		int dayLittle = Integer.parseInt(littleArray[2]);
+
+		int yearBig = Integer.parseInt(bigArray[0]);
+		int monthBig = Integer.parseInt(bigArray[1]);
+		int dayBig = Integer.parseInt(bigArray[2]);
+		if (yearBig > yearLittle ? true : false) {
+			return true;
+		} else if (yearBig == yearLittle ? true : false) {
+			if (monthBig > monthLittle ? true : false) {
+				return true;
+			} else if (monthBig == monthLittle ? true : false) {
+				if (dayBig > dayLittle ? true : false) {
+					return true;
+				} else if (dayBig == dayLittle ? true : false) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
 }
