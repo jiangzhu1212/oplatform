fed0ba652f8cd1d088d8b5646fc362638527a030
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-29 17:21:10 +0000

    接口变更，补全方法

--- src/com/risetek/operation/platform/process/client/model/ProcessData.java
+++ src/com/risetek/operation/platform/process/client/model/ProcessData.java
@@ -4,8 +4,20 @@ import com.risetek.operation.platform.launch.client.model.OPlatformData;
 
 public class ProcessData extends OPlatformData {
 
+	private int sum;
+	
 	public void parseData(String text){
 		setSum(100);
+	}
+
+	@Override
+	public int getSum() {
+		return sum;
+	}
+
+	@Override
+	public void setSum(int sum) {
+		this.sum = sum;
 	}
 	
 }
