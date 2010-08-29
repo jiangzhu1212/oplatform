7ad36407987832f828d8f60d1457855c81b7e570
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-29 17:20:15 +0000

    增加角色管理数据类

---
+++ branches/oplatform.base/src/com/risetek/operation/platform/base/client/model/RoleConfigData.java
@@ -0,0 +1,19 @@
+package com.risetek.operation.platform.base.client.model;
+
+import com.risetek.operation.platform.launch.client.model.OPlatformData;
+
+public class RoleConfigData extends OPlatformData {
+
+	@Override
+	public int getSum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setSum(int sum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
