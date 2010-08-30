d6c28433b408b2a2cd90e4f3d90610ddb132d6ac
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-30 15:39:14 +0000

    抽取子类变为公共类

---
+++ branches/oplatform.launch/src/com/risetek/operation/platform/launch/client/control/DialogControl.java
@@ -0,0 +1,9 @@
+package com.risetek.operation.platform.launch.client.control;
+
+import com.risetek.operation.platform.launch.client.dialog.CustomDialog;
+
+public abstract class DialogControl {
+
+	protected abstract CustomDialog getDialog();
+	
+}
