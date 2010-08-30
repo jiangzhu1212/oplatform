155cf66966d51437b9897220f350ca4553211ef5
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-08-30 08:12:15 +0000

    增加历史记录功能

--- branches/oplatform.launch/src/com/risetek/operation/platform/launch/client/OplatformLaunch.java
+++ branches/oplatform.launch/src/com/risetek/operation/platform/launch/client/OplatformLaunch.java
@@ -7,7 +7,10 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Grid;
@@ -20,6 +23,7 @@ import com.google.gwt.user.client.ui.TreeItem;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.risetek.operation.platform.launch.client.dialog.NoticeDialog;
+import com.risetek.operation.platform.launch.client.sink.SinkInfo;
 
 /**
  * @author Amber
@@ -42,6 +46,9 @@ public abstract class OplatformLaunch implements EntryPoint {
 	 */
 	
 	private HTML noticeHtml = new HTML();
+	final VerticalPanel body = new VerticalPanel();
+	Tree userMenu = new Tree();
+	private SinkInfo curInfo;
 	
 	public void onModuleLoad() {
 		displayNowTime();
@@ -75,6 +82,64 @@ public abstract class OplatformLaunch implements EntryPoint {
 		pubAction.add(blank);
 		pubAction.add(logout);
 		RootPanel.get("public").add(pubAction);
+		
+		History.addValueChangeHandler(new ValueChangeHandler<String>() {
+			public void onValueChange(ValueChangeEvent<String> event) {
+				String token = event.getValue();
+				SinkInfo info = findSinkInfo(token);
+				if(info == null){
+					showInfo();
+					return;
+				}
+				show(info, false);
+			}
+		});
+		
+		String initToken = History.getToken();
+		if(initToken.length()>0){
+			onHistoryChanged(initToken);
+		} else {
+			showInfo();
+		}
+	}
+	
+	public void onHistoryChanged(String token){
+		SinkInfo info = findSinkInfo(token);
+		if(info == null){
+			showInfo();
+			return;
+		}
+		show(info, false);
+	}
+	
+	private SinkInfo findSinkInfo(String token){
+		int count = userMenu.getItemCount();
+		if(count>0){
+			for(int i=0;i<count;i++){
+				TreeItem child = userMenu.getItem(i);
+				int cc = child.getChildCount();
+				if(cc>0){
+					for(int a=0;a<cc;a++){
+						TreeItem cchild = child.getChild(a);
+						Object object = cchild.getUserObject();
+						SinkInfo info = (SinkInfo)object;
+						if(info.getTag().equals(token)){
+							TreeItem parent = cchild.getParentItem();
+							if(!parent.getState()){
+								parent.setState(true);
+							}
+							cchild.setSelected(true);
+							return info;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	private void showInfo(){
+		selectDefaultWidget();
 	}
 	
 	/**
@@ -101,23 +166,24 @@ public abstract class OplatformLaunch implements EntryPoint {
 		leftPanel.setWidth("180px");
 		leftPanel.setHeight("100%");
 		
-		final VerticalPanel body = new VerticalPanel();
 		body.setStyleName("body");
 		body.setWidth("100%");
 		body.setHeight("100%");
 		
-		Tree userMenu = new Tree();
 		userMenu = registerTreeMenu(userMenu);
 		userMenu.addSelectionHandler(new SelectionHandler<TreeItem>() {
 			public void onSelection(SelectionEvent<TreeItem> event) {
 				TreeItem item = event.getSelectedItem();
 				if(item.getChildCount()<1){
 					Object obj = item.getUserObject();
-					if(obj instanceof Widget){
-						body.clear();
-						Widget w = (Widget)obj;
-						body.add(w);
-					}
+					SinkInfo info = (SinkInfo)obj;
+					show(info, true);
+					onHistoryChanged(info.getTag());
+//					if(obj instanceof Widget){
+//						body.clear();
+//						Widget w = (Widget)obj;
+//						body.add(w);
+//					}
 				}
 			}
 		});
@@ -141,7 +207,7 @@ public abstract class OplatformLaunch implements EntryPoint {
 		HorizontalPanel blank = new HorizontalPanel();
 		blank.setStyleName("blank5");
 		
-		Widget notice = creatStackPanel("通知公告", ng);
+		Widget notice = creatStackPanel("内部通知公告", ng);
 		notice.setWidth("180px");
 		
 		leftPanel.add(notice);
@@ -153,7 +219,6 @@ public abstract class OplatformLaunch implements EntryPoint {
 		main.add(leftPanel);
 		main.add(body);
 		main.setCellWidth(leftPanel, "185px");
-		selectDefaultWidget(userMenu, body);
 		return main;
 	}
 	
@@ -162,7 +227,7 @@ public abstract class OplatformLaunch implements EntryPoint {
 	 * @param userMenu
 	 * @param body
 	 */
-	private void selectDefaultWidget(Tree userMenu, VerticalPanel body) {
+	private void selectDefaultWidget() {
 		TreeItem item = userMenu.getItem(0);
 		item.setState(true);
 		if(item.getChildCount()>0){
@@ -172,12 +237,10 @@ public abstract class OplatformLaunch implements EntryPoint {
 			}
 		}
 		item.getUserObject();
+		body.clear();
 		Object obj = item.getUserObject();
-		if(obj instanceof Widget){
-			body.clear();
-			Widget w = (Widget)obj;
-			body.add(w);
-		}
+		SinkInfo info = (SinkInfo)obj;
+		show(info, true);
 	}
 
 	/**
@@ -214,5 +277,18 @@ public abstract class OplatformLaunch implements EntryPoint {
 		stack.setCellHeight(grid, "30px");
 		stack.setCellHeight(w, "100%");
 		return stack;
+	}
+	
+	public void show(SinkInfo info, boolean affectHistory){
+		if(info == curInfo){
+			return;
+		}
+		curInfo = info;
+		body.clear();
+		Widget w = info.getInstance().getWidget();
+		body.add(w);
+		if(affectHistory){
+			History.newItem(info.getTag());
+		}
 	}
 }
