6e3c2ae67e828b8ebe46c93caff96845735733ab
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-09-01 10:13:48 +0000

    新增用户角色服务端处理

---
+++ src/com/risetek/operation/platform/base/server/RoleServiceImpl.java
@@ -0,0 +1,52 @@
+package com.risetek.operation.platform.base.server;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.risetek.operation.platform.base.client.service.RoleService;
+import com.risetek.operation.platform.launch.client.entry.Role;
+import com.risetek.operation.platform.launch.server.core.ConnectDataBase;
+
+public class RoleServiceImpl extends RemoteServiceServlet implements RoleService {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Role[] getAllRole() {
+		Role[] roles = null;
+		List<Role> list = new ArrayList<Role>();
+		try {
+			Connection conn = ConnectDataBase.CONNDB.getConnection();
+			Statement statement = conn.createStatement();
+			String SQL = "select * from risetek_role order by id";
+			ResultSet result = statement.executeQuery(SQL);
+			while(result.next()){
+				Role role = new Role();
+				role.setId(result.getInt(1));
+				role.setRoleName(result.getString(2));
+				list.add(role);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if(list.size()>0){
+			roles = new Role[list.size()];
+			for(int i=0;i<roles.length;i++){
+				roles[i] = list.get(i);
+			}
+		} else {
+			roles = new Role[0];
+		}
+		return roles;
+	}
+
+}
