f54da8e905b21f7b5c98016f5bf420e70783a7a9
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-09-02 16:22:29 +0000

    新增用户管理模块

--- src/com/risetek/operation/platform/base/client/UserConfigSink.java
+++ src/com/risetek/operation/platform/base/client/UserConfigSink.java
@@ -1,28 +1,41 @@
 package com.risetek.operation.platform.base.client;
 
 import com.google.gwt.user.client.ui.Widget;
+import com.risetek.operation.platform.base.client.control.UserConfigController;
+import com.risetek.operation.platform.base.client.view.UserConfigView;
 import com.risetek.operation.platform.launch.client.control.AController;
 import com.risetek.operation.platform.launch.client.sink.Sink;
 import com.risetek.operation.platform.launch.client.sink.SinkInfo;
 
 public class UserConfigSink extends Sink {
 
+	public static final String Group = "系统管理";
+	public static final String Tag = "userconfig";
+	public static final String Name = "用户管理";
+	public static final String Desc = "管理登录本平台的内部用户";
+	
+	public static SinkInfo init() {
+		return new SinkInfo(Group, Tag, Name, Desc) {
+			public Sink createInstance() {
+				return new UserConfigSink();
+			}
+		};
+	}
+	
 	@Override
 	public AController getController() {
-		// TODO Auto-generated method stub
-		return null;
+		return UserConfigController.INSTANCE;
 	}
 
 	@Override
 	public Widget getWidget() {
-		// TODO Auto-generated method stub
-		return null;
+		UserConfigView.descript = Desc;
+		return UserConfigController.INSTANCE.view;
 	}
 
 	@Override
 	public SinkInfo getSinkInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		return init();
 	}
 
 }
---
+++ src/com/risetek/operation/platform/base/client/view/UserConfigView.java
@@ -0,0 +1,37 @@
+package com.risetek.operation.platform.base.client.view;
+
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.xml.client.Node;
+import com.risetek.operation.platform.launch.client.model.OPlatformData;
+import com.risetek.operation.platform.launch.client.view.IOPlatformView;
+import com.risetek.operation.platform.launch.client.view.OPlatformTableView;
+
+public class UserConfigView extends OPlatformTableView implements IOPlatformView {
+
+	public static String descript = "";
+	
+	@Override
+	public void ProcessControlKey(int keyCode, boolean alt) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String[] parseRow(Node node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Grid getGrid() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void render(OPlatformData data) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
