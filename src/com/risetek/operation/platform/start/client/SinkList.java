7484921b84062dc2ae4f79ad9c3d041ec738d8b3
jiangzj520 <jiangzj520@832c6b46-9473-f359-a91a-35190615d28e>
2010-09-02 09:56:47 +0000

    增加全国充值管理界面以及基础代码

--- src/com/risetek/operation/platform/start/client/SinkList.java
+++ src/com/risetek/operation/platform/start/client/SinkList.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import com.risetek.operation.platform.base.client.AcountSink;
 import com.risetek.operation.platform.base.client.AnnoucementSink;
+import com.risetek.operation.platform.base.client.BankSink;
 import com.risetek.operation.platform.base.client.BaseSink;
+import com.risetek.operation.platform.base.client.Card007Sink;
 import com.risetek.operation.platform.base.client.CustomerSink;
 import com.risetek.operation.platform.base.client.RoleConfigSink;
 import com.risetek.operation.platform.base.client.TdbcSink;
@@ -33,10 +35,12 @@ public class SinkList {
 		sinkList.add(new ProcessSink());
 		sinkList.add(new CustomerSink());
 		sinkList.add(new TransactionSink());
+		sinkList.add(new BankSink());
 		sinkList.add(new AcountSink());
 		sinkList.add(new TdbcSink());
-		sinkList.add(new TransEnableSink());
 		sinkList.add(new AnnoucementSink());
+		sinkList.add(new Card007Sink());
+		sinkList.add(new TransEnableSink());
 		sinkList.add(new RoleConfigSink());
 		sinkList.add(new TransBindSink());
 		return sinkList;
