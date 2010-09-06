9581823066f7eb4b35d2be4215ea773404f3b0eb
onlyyouabcd <onlyyouabcd@832c6b46-9473-f359-a91a-35190615d28e>
2010-09-06 11:39:11 +0000

    增加骏网管理功能

---
+++ src/com/risetek/operation/platform/launch/client/json/constanst/PBabyConstanst.java
@@ -0,0 +1,30 @@
+package com.risetek.operation.platform.launch.client.json.constanst;
+
+public interface PBabyConstanst extends Constanst{
+	
+	final String IS_OK = "IS_OK";
+	
+	final String IS_OK_ZH = "是否出票成功";
+	
+	final String ORDID = "OrdID";
+	
+	final String ORDID_ZH = "帐号";
+	
+	final String KEYID = "KeyID";
+	
+	final String KEYID_ZH = "密码";
+	
+	final String CODE = "Code";
+	
+	final String CODE_ZH = "航班号";
+	
+	final String SEAT = "Seat";
+	
+	final String SEAT_ZH = "帐号";
+	
+	final String PNR = "PNR";
+	
+	final String PNR_ZH = "PNR";
+	
+	final String USER_VALUE = "polyvi";
+}
