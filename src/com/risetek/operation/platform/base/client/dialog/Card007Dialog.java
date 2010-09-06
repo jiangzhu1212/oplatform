771e01056f93063f4087d9734eda121ba1bd7a3a
jiangzj520 <jiangzj520@832c6b46-9473-f359-a91a-35190615d28e>
2010-09-06 02:45:03 +0000

    添加全国充值代码注释

--- src/com/risetek/operation/platform/base/client/dialog/Card007Dialog.java
+++ src/com/risetek/operation/platform/base/client/dialog/Card007Dialog.java
@@ -1,6 +1,8 @@
 package com.risetek.operation.platform.base.client.dialog;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
@@ -12,32 +14,31 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.datepicker.client.DateBox;
 import com.risetek.operation.platform.launch.client.json.constanst.BillCard007;
+import com.risetek.operation.platform.launch.client.util.Util;
 
 /**
- * @author asus
- *
+ * @ClassName: Card007Dialog 
+ * @Description: 全国充值 （007）事件实体类 
+ * @author JZJ 
+ * @date 2010-9-6 上午09:17:05 
+ * @version 1.0
  */
 public class Card007Dialog extends BaseDialog { 
 	
-	public final static DateBox dateBox = new DateBox();
-	private final ListBox timeBox = new ListBox();
-	public static String timeValue = "";
-	private final static String[] timeVale = { "", "12:00", "12:15", "12:30",
-			"12:45", "1:00", "1:15", "1:30", "1:45", "2:00", "2:15", "2:30",
-			"2:45", "3:00", "3:15", "3:30", "3:45", "4:00", "4:15", "4:30",
-			"4:45", "5:00", "5:15", "5:30", "5:45", "6:00", "6:15", "6:30",
-			"6:45", "7:00", "7:15", "7:30", "7:45", "8:00", "8:15", "8:30",
-			"8:45", "9:00", "9:15", "9:30", "9:45", "10:00", "10:15", "10:30",
-			"10:45", "11:00", "11:15", "11:30", "11:45" };
-	
-	public Card007Dialog(){}
+	public DateBox dateBox = new DateBox();
+	public String timeValue = "";
 	
 	public Card007Dialog(Grid grid, int row){
-		makeMainPanel(grid , row);
-		createListBox();
+		createMainPanel(grid , row);
 	}
 	
-	public void makeMainPanel(Grid grid, int row) {
+	/**
+	 * @Description: 创建充值面板
+	 * @param grid
+	 * @param row  参数 
+	 * @return void 返回类型
+	 */
+	public void createMainPanel(Grid grid, int row) {
 		int colCount = grid.getColumnCount();
 		if (colCount < 3) {
 			Window.alert("错误");
@@ -53,35 +54,69 @@ public class Card007Dialog extends BaseDialog {
 		
 		HorizontalPanel hp = new HorizontalPanel();
 		hp.add(dateBox);
-		hp.add(timeBox);
-		gridFrame.setWidget(i, 0, new Label("充值时间："));
+		hp.add(createListBox());
+		gridFrame.setWidget(i, 0, new Label("充值日期："));
 		gridFrame.setWidget(i, 1, hp);
 		gridFrame.getCellFormatter().setHorizontalAlignment(i, 0, HasHorizontalAlignment.ALIGN_RIGHT);
-		
+
 		dateBox.setFormat(new DateBox.DefaultFormat(BaseDialog.format));
-		dateBox.setValue(new Date());
 		dateBox.setWidth("80px");
-		timeBox.setWidth("70px");
+		createListBox().setWidth("70px");
 		gridFrame.setWidth("350px");
 		setText("全国充值");
 		setDescript("请输入充值时间");
 		mainPanel.add(gridFrame);
 	}
 	
-	
-	private void createListBox(){
-		for(int i=0;i<timeVale.length;i++){
-			timeBox.addItem(timeVale[i]);
+	/**
+	 * @Description: 创建时间选择下拉框
+	 * @return  参数 
+	 * @return ListBox 返回类型
+	 */
+	@SuppressWarnings("rawtypes")
+	private ListBox createListBox() {
+		final ListBox timeBox = new ListBox();
+		final List list = getTime();
+		for (Object obj : list) {
+			timeBox.addItem((obj.toString()));
 		}
+		
 		timeBox.addChangeHandler(new ChangeHandler() {
 			@Override
 			public void onChange(ChangeEvent event) {
 				int index = timeBox.getSelectedIndex();
-				timeValue = timeVale[index];
+				timeValue = (list.get(index)).toString();
+				Window.alert(timeValue);
 			}
 		});
+		return timeBox;
 	}
 	
+	/**
+	 * @Description: 组合时间数据
+	 * @return  参数 
+	 * @return List 返回类型
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private List getTime() {
+		List time = new ArrayList();
+		time.add("");
+		for (int h = 1; h <= 12; h++) {
+			time.add(h + ":00");
+			for (int m = 15; m <= 45; m += 15) {
+				time.add(h + ":" + m);
+			}
+		}
+		return time;
+	}
+	
+	/**
+	 * @Description: 返回需要发送的JSON数据
+	 * @param grid
+	 * @param row
+	 * @return  参数 
+	 * @return BillCard007 返回类型
+	 */
 	public BillCard007 getData(Grid grid, int row){
 		BillCard007 card = new BillCard007();
 		card.setCHARGE_PHONE_NUMBER(grid.getText(row, 2));
@@ -94,6 +129,20 @@ public class Card007Dialog extends BaseDialog {
 		return card;
 	}
 	
+	public String getChargeDateTime(){
+		String dateTime = "";
+		Date date = dateBox.getValue();
+		if(null != date && null != timeValue && !"".equals(timeValue)){
+			dateTime = Util.formatDateToJsonString(dateBox.getValue()) + Util.formatHmStringTime(timeValue);
+		}
+		return dateTime;
+	}
+	
+	/**
+	 * @Description:  验证
+	 * @return  参数 
+	 * @return boolean 返回类型
+	 */
 	public boolean isValid() {
 		String date = dateBox.getTextBox().getText().trim();
 		if(null == date || "".equals(date)){
