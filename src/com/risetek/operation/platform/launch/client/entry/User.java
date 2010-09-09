3c7d49ad90ffe9765048ce3d52667a3b2a236682
hupoos@gmail.com <hupoos@gmail.com@832c6b46-9473-f359-a91a-35190615d28e>
2010-09-09 03:09:14 +0000

    整理代码

---
+++ src/com/risetek/operation/platform/launch/client/entry/User.java
@@ -0,0 +1,113 @@
+package com.risetek.operation.platform.launch.client.entry;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	private String userName;
+	
+	private String userPassword;
+	
+	private Integer status;
+	
+	private Integer role;
+	
+	private String email;
+	
+	private Timestamp lastLoginTime;
+	
+	private String lastLoginAddress;
+	
+	private Timestamp registTime;
+	
+	private String notes;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Timestamp getLastLoginTime() {
+		return lastLoginTime;
+	}
+
+	public void setLastLoginTime(Timestamp lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
+
+	public String getLastLoginAddress() {
+		return lastLoginAddress;
+	}
+
+	public void setLastLoginAddress(String lastLoginAddress) {
+		this.lastLoginAddress = lastLoginAddress;
+	}
+
+	public Timestamp getRegistTime() {
+		return registTime;
+	}
+
+	public void setRegistTime(Timestamp registTime) {
+		this.registTime = registTime;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+}
